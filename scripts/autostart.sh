b56f796030e0def4848cf538405b2daccb3c3f0a
aizhiqi <zhiqi.ai@gmail.com>
2022-11-18 19:37:02 +0800

    Add files via upload
    init commit

---
+++ scripts/autostart.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+xrdb -merge ~/.config/bspwm/xresources/xresources
+
+xsetroot -cursor_name left_ptr
+
+alias rofi='rofi -config=~/.config/bspwm/rofi/config.rasi -show drun'
+alias ranger='ranger --confdir=~/.config/bspwm/ranger'
+
+export GTK_IM_MODULE=fcitx
+export QT_IM_MODULE=fcitx
+export XMODIFIERS=@im=fcitx
+
+
+picom --config ~/.config/bspwm/picom/picom.conf &
+
+arr=("copyq" "fcitx" "xdman" "redshift-gtk" "picom" "conky"\
+	"~/.config/bspwm/bin/bspbar"\
+	"~/.config/bspwm/bin/bsphotkey"\
+	"~/.config/bspwm/bin/bspnotifier"\
+	"~/.config/bspwm/bin/bspwallpaper"\
+)
+
+for value in ${arr[@]}
+do
+    isExist=`ps -ef | grep "$value" | grep -v grep | wc -l`
+    if [ $isExist == 0 ]
+    then
+        exec "$value" &
+    fi
+done
+
+xmodmap ~/.config/bspwm/xmodmap/xmodmap
